9e0306f828f73bf12197453cc074c0fee539dca2
Oujiii <34745265+Oujiii@users.noreply.github.com>
2019-08-21 19:58:19 -0300

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#install docker and docker-compose
+
+#Get the script install from Docker website and execute it
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+echo "Docker was installed"
+
+#Get the script install from Docker website and execute it
+curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+echo "Docker-compose was installed"
